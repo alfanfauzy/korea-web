2a59e2693906c476662031c5f082ba51850ffb06
alfanfauzy <alfan.fauzy@moduit.id>
2024-10-24 15:57:59 +0700

    add title web

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Hypehire - NextJS - TailwindCSS</title>
+        <meta
+          name="description"
+          content="Slice design with NextJS and TailwindCSS"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
